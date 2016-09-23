9af9251a6ffb73d8a9bf3a4281f865fa10508138
pattydiaz <pdiaz@pdiazdesign.com>
2016-09-23 12:17:07 -0400

    initial commit

---
+++ comparison/js/dropdown.js
@@ -0,0 +1,8 @@
+$( function() {
+    $( "#veh2" ).selectmenu({
+		  position: { my : "top+20%", at: "center" }
+		});
+    $( "#veh3" ).selectmenu({
+		  position: { my : "top+20%", at: "center" }
+		});
+} );
