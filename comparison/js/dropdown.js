330909318782aaf17b43fddac419156383bdb70a
mauricioalfonso007 <mauricioalfonso007@gmail.com>
2016-09-23 11:59:47 -0500

    Add files via upload

--- comparison/js/dropdown.js
+++ comparison/js/dropdown.js
@@ -1,8 +1,16 @@
 $( function() {
     $( "#veh2" ).selectmenu({
-		  position: { my : "top+20%", at: "center" }
+		   position: { my : "top+20%", at: "center" },
+		    change: function( event, data ) {
+		         change_vehicle();
+		       }
 		});
     $( "#veh3" ).selectmenu({
-		  position: { my : "top+20%", at: "center" }
+		    position: { my : "top+20%", at: "center" },
+		  	 change: function( event, data ) {
+        		 change_vehicle();
+      		 }
 		});
+
+
 } );
