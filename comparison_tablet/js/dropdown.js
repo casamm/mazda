868a2a18ae6bce1c813ce78f9d814d41852c0615
mauricioalfonso007 <mauricioalfonso007@gmail.com>
2016-10-11 09:30:46 -0500

    Update js

--- comparison_tablet/js/dropdown.js
+++ comparison_tablet/js/dropdown.js
@@ -2,10 +2,22 @@ $( function() {
     $( "#veh2" ).selectmenu({
 		   position: { my : "top+20%", at: "center" },
 		    change: function( event, data ) {
-		         change_vehicle();
+		         change_tipe();
 		       }
 		});
     $( "#veh3" ).selectmenu({
+		    position: { my : "top+20%", at: "center" },
+		  	 change: function( event, data ) {
+        		 change_tipe();
+      		 }
+		});
+     $( "#tipveh2" ).selectmenu({
+		   position: { my : "top+20%", at: "center" },
+		    change: function( event, data ) {
+		         change_vehicle();
+		       }
+		});
+    $( "#tipveh3" ).selectmenu({
 		    position: { my : "top+20%", at: "center" },
 		  	 change: function( event, data ) {
         		 change_vehicle();
