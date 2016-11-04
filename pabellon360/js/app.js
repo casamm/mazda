36f6650e6a7ded0ec52860fe01c601799efd4815
pattydiaz <pdiaz@pdiazdesign.com>
2016-11-04 16:07:55 -0400

    desktop style changes

--- pabellon360/js/app.js
+++ pabellon360/js/app.js
@@ -1,48 +1,57 @@
-function CloseOverlay()
-{
-	$("#iframe-content").hide();  
-	document.getElementById('iframe-content').src = "../vehicles/index.html";
+function CloseOverlay() {
+    $("#iframe-content").hide();
+    
+    document.getElementById('iframe-content').src = "../vehicles/index.html";
 
-    $("#divmenu").addClass("hidden");
     $("#iframe-content").addClass("hidden");
-    $(".desktop header .wrap").removeClass("invisible");
+    $(".desktop .menu").addClass("invisible").animate({ opacity: 0 });
+
+    $(".page-title").each(function(i, el) {
+            $(el).removeClass('hidden');
+        })
+
 }
 
-function ChangeContent(idcontent)
-{
-$("#iframe-content").show();
-$("#iframe-content-phone").show();
+function ChangeContent(event, idcontent) {
+    $("#iframe-content").show();
+    $("#iframe-content-phone").show();
 
-	switch(idcontent) 
-	{
-	case 0:
-	    $("#iframe-content").hide();
-        $("#iframe-content-phone").hide();
-        break;
-    case 1:
-        document.getElementById('iframe-content').src = "../sessions/index.php";
-        document.getElementById('iframe-content-phone').src = "../sessions/index.php";
-        break;
-    case 2:
-        document.getElementById('iframe-content').src = "../vehicles/index.html";
-        document.getElementById('iframe-content-phone').src = "../vehicles/index.html";
-        break;
-    case 3:
-        document.getElementById('iframe-content').src = "../comparison/index.html";
-        document.getElementById('iframe-content-phone').src = "../comparison/index.html";
-        break;
-    case 4:
-        document.getElementById('iframe-content').src = "../guide/index.html";
-        document.getElementById('iframe-content-phone').src = "../guide/index.html";
-        break;
-	}
-        $("#divmenu").removeClass("hidden");
-        $("#iframe-content").removeClass("hidden");
-        $(".desktop header .wrap").addClass("invisible");
+    $(".page-title").each(function(i, el) {
+        if (el !== event.target) {
+            $(el).addClass('hidden');
+        }
+    })
 
-	   $(".bar").toggleClass("animation");
-	   $(".menu-overlay").toggleClass("show");
-	   $(".desktop .title").toggleClass("invisible");
-	   $("header").toggleClass("background");
+    switch (idcontent) {
+        case 0:
+            $("#iframe-content").hide();
+            $("#iframe-content-phone").hide();
+            break;
+        case 1:
+            document.getElementById('iframe-content').src = "../sessions/index.php";
+            document.getElementById('iframe-content-phone').src = "../sessions/index.php";
+            break;
+        case 2:
+            document.getElementById('iframe-content').src = "../vehicles/index.html";
+            document.getElementById('iframe-content-phone').src = "../vehicles/index.html";
+            break;
+        case 3:
+            document.getElementById('iframe-content').src = "../comparison/index.html";
+            document.getElementById('iframe-content-phone').src = "../comparison/index.html";
+            break;
+        case 4:
+            document.getElementById('iframe-content').src = "../guide/index.html";
+            document.getElementById('iframe-content-phone').src = "../guide/index.html";
+            break;
+    }
+
+    $("#iframe-content").removeClass("hidden");
+
+    $(".phone .bar").toggleClass("animation");
+    $(".phone .menu-overlay").toggleClass("show");
+    $(".phone header").toggleClass("background");
+
+    $(".desktop .menu").removeClass("invisible").animate({ opacity: 1 });
+
 
 }
